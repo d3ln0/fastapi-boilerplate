d0401316e85932fff782066eb3d1563606a850e8
Noah Riedel <git@riedel-noah.de>
2023-07-13 14:19:06 +0200

    add sqlmodel to alembic env

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
-import lib.models
+from lib.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
