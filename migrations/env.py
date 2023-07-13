27d06fd481ea1ce60de5c8222249cea83e9b0b99
Noah Riedel <git@riedel-noah.de>
2023-07-13 14:09:52 +0200

    add sqlmodel to alembic env

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,11 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlmodel import SQLModel
 
 from alembic import context
+
+import lib.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
